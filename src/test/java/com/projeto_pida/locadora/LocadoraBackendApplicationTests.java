f1f753deb16f9f98512f93a76ccb3a2c4962ee15
Antonio Samuel <antoniosamu99112@gmail.com>
2026-04-21 18:16:04 -0300

    Initial commit: Estrutura Spring Boot 3.5.13 e Java 21

---
+++ src/test/java/com/projeto_pida/locadora/LocadoraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto_pida.locadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadoraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
